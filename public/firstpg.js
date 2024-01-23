380c173dffd7868905686b65a6d5472b5b903974
sadia161 <144538429+sadia161@users.noreply.github.com>
2024-01-23 23:57:27 +0600

    deleted

--- public/firstpg.js
+++
@@ -1,9 +0,0 @@
-function redirectToLogin(userType) {
-    if (userType === 'customer') {
-        window.location.href = 'login.html';
-    } else if (userType === 'hotelowner') {
-        window.location.href = 'loginowner.html';
-    } else {
-        alert('Invalid user type selected.');
-    }
-}
